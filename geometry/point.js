2d1fa06447fb177e6b4909fc1c906c42061086c3
Evan Brierton <evanbrierton@icloud.com>
2019-12-19 15:51:06 +0000

    Formatting

--- geometry/point.js
+++ geometry/point.js
@@ -4,12 +4,12 @@ class Point {
   constructor({
     x, y, radius, angle,
   }) {
-    this.cartesian = {x: x || radius * Math.cos(angle), y: y || radius * Math.sin(angle)}
+    this.cartesian = { x: x || radius * Math.cos(angle), y: y || radius * Math.sin(angle) };
 
-    this.polar = { radius: radius || modulus(x, y), angle: angle || y === 0 ? 0 : Math.atan2(y, x) };
+    this.polar = {
+      radius: radius || modulus(x, y), angle: angle || y === 0 ? 0 : Math.atan2(y, x),
+    };
   }
 }
-
-console.log(Math.atan2(0, 2))
 
 module.exports = Point;
